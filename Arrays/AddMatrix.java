1142951df9fe06c1481335846cb7e666f8ccfd96
sachin-nucleusteq <sachin.rajpoot@nucleusteq.com>
2023-07-20 15:20:21 +0530

    rr added

---
+++ Arrays/AddMatrix.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class AddMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] arr2 = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+
+        
+    }
+}
